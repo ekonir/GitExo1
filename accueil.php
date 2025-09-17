4be4fecd5b6d7442b7d4cf888609eb51534b5077
Lea PISANESCHI-RENAULT <lpisa@lmdsio.com>
2025-09-17 15:41:03 +0200

    version 1

---
+++ accueil.php
@@ -0,0 +1,11 @@
+<?php
+// récupération des zones de saisie dans des variables locales
+$prenom = $_POST["prenom"];
+$nom = $_POST["nom"];
+// affichage
+echo "Bonjour $prenom $nom ! <br/>";
+?>
+
+
+
+
